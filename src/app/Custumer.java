539931dba70e342e6b8647ebe004d226f1baac77
Marys888 <severovamaha1999@gmail.com>
2025-04-13 11:10:38 +0200

    Add temporary Custumer info code to test

--- src/app/Custumer.java
+++ src/app/Custumer.java
@@ -1,5 +1,31 @@
 package app;
 
 public class Custumer {
+    private  String name;
+    private String phone;
 
+    public Custumer(String name, String phone) {
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public String getName() {
+
+        return name;
+    }
+
+    public void setName(String name) {
+
+        this.name = name;
+    }
+
+    public String getPhone() {
+
+        return phone;
+    }
+
+    public void setPhone (String phone) {
+
+        this.phone = phone;
+    }
 }
--- src/app/Main.java
+++ src/app/Main.java
@@ -3,5 +3,7 @@ package app;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Custumer Info app");
+        Custumer custumerTest = new Custumer("Tom", "555 123-8596");
+        System.out.println("Custumer: " + custumerTest.getName() + "," + " " + "phone" + " " + custumerTest.getPhone());
     }
 }
